e3ea85625a1f63cac432f721112af9495c1cf372
Pablo096 <ben19_kad@hotmail.com>
2020-03-24 10:21:27 -0600

    Update practica6.py

--- practica6.py
+++ practica6.py
@@ -12,20 +12,44 @@ from random import choice
 
 class Cajero(KnowledgeEngine):
         
-    @Rule( AND(AND(AND(OR(Fact(tarjeta='funcional'), Fact(cuentaBancaria='si')),Fact(nip='aceptado')), Fact(saldo='suficiente')),Fact(saldo_cajero='suficiente')) )
+    @Rule( AND(
+               AND(
+                   AND(
+                       OR(
+                          Fact(tarjeta='funcional'), Fact(cuentaBancaria='si')
+                          ),
+                       Fact(nip='aceptado')
+                       ), 
+                   Fact(saldo='suficiente')
+                 ),Fact(saldo_cajero='suficiente')
+                ) 
+           )
     def retiroExitoso(self):
         print("El retiro ha sido exitoso")
         
-    @Rule( OR(OR(OR(AND(Fact(tarjeta='no_funcional'), Fact(cuentaBancaria='no')),Fact(nip='no_aceptado')),Fact(saldo='insuficiente')), Fact(saldo_cajero='insuficiente')) )
+    @Rule( OR(
+              OR(
+                 OR(
+                    AND(
+                        Fact(tarjeta='no_funcional'), Fact(cuentaBancaria='no')
+                        ),
+                    Fact(nip='no_aceptado')
+                    ),
+                 Fact(saldo='insuficiente')
+                 ),
+              Fact(saldo_cajero='insuficiente')
+              ) 
+          )
     def retiroNoExitoso(self):
         print("El retiro NO ha sido exitoso")
 
 
 engine = Cajero()
 engine.reset()
+
 engine.declare(Fact(tarjeta = choice(['funcional','no_funcional'])))
 engine.declare(Fact(cuentaBancaria = choice(['si','no'])))
-engine.declare(Fact(nip = choice(['acep tado','no_aceptado'])))
+engine.declare(Fact(nip = choice(['aceptado','no_aceptado'])))
 engine.declare(Fact(saldo = choice(['suficiente','insuficiente'])))
 engine.declare(Fact(saldo_cajero = choice(['suficiente','insuficiente'])))
 
